f23651115e8642c046510f681c4ddaccab445372
Nastya Shmidt <nshmidt2005@gmail.com>
2022-01-31 23:13:15 +0300

    загрузка пз10

--- Pz_10/Pz_10_1.py
+++ Pz_10/Pz_10_1.py
@@ -3,8 +3,8 @@
 # текстовый файл(.txt) следующего вида, предварительно выполнив требуемую обработку элементов:
 # Элементы первого и второго файлов:
 # Количество элементов первого и второго файлов:
-# Индекс первого минимального элемента первого файла:
-# Индекс последнего максимального элемента второго файла:
+# Индекс минимального элемента первого файла:
+# Индекс максимального элемента второго файла:
 # Элементы кратные 4 первого и второго файлов:
 
 import random
@@ -15,7 +15,7 @@ a = []
 b = []
 f1 = open("One.txt","w", encoding="utf8")
 for i in range(10):
-    a.append(random.randint(1,10))
+    a.append(random.randint(-10,10))
     f1.write(str(a[i]))
     f1.write(" ")
     x += 1
@@ -23,7 +23,7 @@ f1.close()
 
 f2 = open("Two.txt","w", encoding="utf8")
 for i in range(10):
-    b.append(random.randint(-10,-1))
+    b.append(random.randint(-10,10))
     f2.write(str(b[i]))
     f2.write(" ")
     y += 1
@@ -43,18 +43,27 @@ f3 = open("Three.txt","w", encoding="utf8")
 f3.write("Элементы первого и второго файлов: ")
 f3.write(str(g))
 f3.write("\n")
+
 f3.write("Количество элементов первого и второго файлов: ")
 f3.write(str(k))
 f3.write("\n")
-f3.write("Индекс первого минимального элемента первого файла: ")
-f3.write(str())
+
+f3.write("Индекс минимального элемента первого файла: ")
+min(a)
+f3.write(str(a.index(min(a))))
+f3.write('\n')
+
+f3.write("Индекс максимального элемента второго файла: ")
+max(b)
+f3.write(str(b.index(max(b))))
 f3.write("\n")
-f3.write("Индекс последнего максимального элемента второго файла: ")
-f3.write(str())
-f3.write("\n")
+
 f3.write("Элементы кратные 4 первого и второго файлов: ")
-for i in range
-    f3.write(str([])
+for i in a:
+    if i % 4 == 0:
+        a.append(i)
+for i in b:  if i % 4 == 0:
+        b.append(i)
 f3.write(" ")
 f3.close()
 
