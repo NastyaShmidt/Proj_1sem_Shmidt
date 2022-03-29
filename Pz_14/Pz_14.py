3732397bdbbab560e401a28e64c3df455575346e
Nastya Shmidt <nshmidt2005@gmail.com>
2022-03-29 12:10:49 +0300

    пз 14

---
+++ Pz_14/Pz_14.py
@@ -0,0 +1,9 @@
+import re
+
+p = re.compile(r"[/:]+")
+s = re.compile(r"[\w]+[.][\w]+[.][\w]+")
+with open('radio_stations.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+reg = re.split(p,text)
+print("Выбранные домены: ")
+print(s.findall(str(reg)))
